07f41b217d0951e1382361c5072b24f3dbe12a28
euericmelo <euericmelo@gmail.com>
2025-10-29 10:35:21 -0300

    versao 1.0

---
+++ JS/ESTRUTURAS_COND/script.js
@@ -0,0 +1,44 @@
+// ESTRUTURAS SIMPLES
+/*
+OPERADORES CONDICIONAIS
+
+== IGUAL Á;
+!= DIFERENTE;
+<  MENOR Q;
+> MAIOR Q;
+<= MENOR OUIGUAL A;
+>= MAIOR OU IGUAL A;
+
+IF E ELSE
+if = SE ,
+else = senao,
+
+*/
+
+   /* const interruptor = "desligado";
+    if (interruptor == "ligado"){
+        console.log("A luz esta acesa")}}
+// copara superficial
+        const a = '10';
+        if ( a == 10){
+            console.log('a é 10')}
+//Compara tipo de dados string ou number
+      const b = '10';
+        if ( a === 10){
+            console.log('a é 10')}
+           */
+
+    /*const nome = "E"
+    if (nome != "Eric")
+console.log("Saia Daqui")
+     
+    //const idade = 16;
+    if (idade < 18)
+        console.log("Ah que pena, Voçê não pode acessar o evento")
+//const idade = 16;
+if (idade > 18){
+    console.log("Ok, Voçê pode entrar no evento")}
+
+    const idade = 16;
+if (idade >= 10){
+    console.log("Ok, Voçê pode entrar no evento")}*/
